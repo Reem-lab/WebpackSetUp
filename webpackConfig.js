c498bbd0a7f6afdedadd662304e95988c88bc543
Reem <janinareem@gmail.com>
2022-01-11 20:18:27 +0200

    Add webpack pakage and library

---
+++ webpackConfig.js
@@ -0,0 +1,33 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+ module.exports = {
+   mode: 'production',
+   devtool: 'inline-source-map',
+   entry: {
+     index: './src/index.js',
+   },
+   module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+  ],
+   output: {
+     filename: '[name].bundle.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
+
+ };
